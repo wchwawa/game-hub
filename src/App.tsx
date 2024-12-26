bf45d1e2472da05d71db6aae60cd29debeef9711
wchwawa <wch19961116@gmail.com>
2024-12-26 13:11:27 +0800

    feat(NavBar): build basic nav bar, add logo picture on nav bar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
 		<div className="App">
-			<Button color={"blue.400"} bgColor={"Highlight"}>
-				hi
-			</Button>
+			<Grid
+				templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+			>
+				<GridItem area="nav">
+					<NavBar></NavBar>
+				</GridItem>
+
+				<Show above="lg">
+					<GridItem area="aside" bg={"gold"}>
+						Aside
+					</GridItem>
+				</Show>
+
+				<GridItem area="main" bg={"dodgerblue"}>
+					Main
+				</GridItem>
+			</Grid>
 		</div>
 	);
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { HStack, Image, Text } from '@chakra-ui/react'
+import React from 'react'
+import logo from '../assets/GameHub Resources/Logo/logo.webp'
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px"></Image>
+      <Text>NavBar</Text>
+    </HStack>
+  )
+}
+
+export default NavBar
