7880b2acab1a5486f5ceafe3780c127a56818148
wchwawa <wch19961116@gmail.com>
2024-12-26 17:00:17 +0800

    feat(services): add function to cropped image from origin url

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,8 +1,17 @@
 import { Game } from "../model/game";
-import { Card, CardBody, Heading, HStack, Icon, Image, Text } from "@chakra-ui/react";
+import {
+	Card,
+	CardBody,
+	Heading,
+	HStack,
+	Icon,
+	Image,
+	Text,
+} from "@chakra-ui/react";
 import { IconType } from "react-icons";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
 	game: Game;
@@ -11,16 +20,16 @@ interface Props {
 const GameCard = ({ game }: Props) => {
 	return (
 		<Card borderRadius={10} overflow={"hidden"}>
-			<Image src={game.background_image}></Image>
+			<Image src={getCroppedImageUrl(game.background_image)}></Image>
 			<CardBody>
 				<Heading fontSize={"2xl"}>{game.name}</Heading>
 
-        <HStack justifyContent={'space-between'}>
-				<PlatformIconList
-					platforms={game.parent_platforms.map((p) => p.platform)}
-				></PlatformIconList>
-        <CriticScore score={game.metacritic}></CriticScore>
-        </HStack>
+				<HStack justifyContent={"space-between"}>
+					<PlatformIconList
+						platforms={game.parent_platforms.map((p) => p.platform)}
+					></PlatformIconList>
+					<CriticScore score={game.metacritic}></CriticScore>
+				</HStack>
 			</CardBody>
 		</Card>
 	);
