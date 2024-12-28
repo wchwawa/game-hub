5b0200955425aaf2fa003c47b6f36aae901e2b71
wchwawa <wch19961116@gmail.com>
2024-12-28 20:04:13 +0800

    fix: remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,15 @@ import App from "./App";
 import "./index.css";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	// <React.StrictMode>
-		<ChakraProvider theme={theme}>
-			<ColorModeScript
-				initialColorMode={theme.config.initialColorMode}
-			></ColorModeScript>
-			<App />
-		</ChakraProvider>
-	// {/* </React.StrictMode> */}
+		<HashRouter>
+			<ChakraProvider theme={theme}>
+				<ColorModeScript
+					initialColorMode={theme.config.initialColorMode}
+				></ColorModeScript>
+				<App />
+			</ChakraProvider>
+		</HashRouter>
 );
