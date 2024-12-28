2330818955c379f957f67f8124d839bbff156bcb
wchwawa <wch19961116@gmail.com>
2024-12-28 17:14:51 +0800

    feat(theme): tuning light and dark theme

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,13 +1,25 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
-
+import { HStack, Icon, Switch, useColorMode } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
 
 const ColorModeSwitch = () => {
 	const { toggleColorMode, colorMode } = useColorMode();
 
 	return (
-		<HStack >
-			<Switch colorScheme="purple" isChecked={colorMode === 'dark'} onChange={toggleColorMode}/>
-			<Text whiteSpace='nowrap'>Dark Mode</Text>
+		<HStack>
+			<Switch
+				colorScheme="purple"
+				isChecked={colorMode === "dark"}
+				onChange={toggleColorMode}
+			/>
+			<motion.div
+				key={colorMode}
+				initial={{ opacity: 0, rotate: -180 }}
+				animate={{ opacity: 1, rotate: 0 }}
+				transition={{ duration: 0.3 }}
+			>
+				<Icon as={colorMode === "dark" ? BsFillSunFill : BsFillMoonFill} />
+			</motion.div>
 		</HStack>
 	);
 };
--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,45 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { mode, StyleFunctionProps } from '@chakra-ui/theme-tools';
 
 const config: ThemeConfig = {
-  initialColorMode: 'dark'
-}
+  initialColorMode: "dark",
+};
 
-const theme = extendTheme({config});
+const colors = {
+  gray: {
+    50: '#e1edec',
+    100: '#e2e3dd',
+    200: '#d9e2e3',
+    300: '#a6a6a6',
+    400: '#8c8c8c',
+    500: '#737373',
+    600: '#595959',
+    700: '#131212',
+    800: '#0b0b0b',
+    900: '#0d0d0d',
+  },
+};
+
+const styles = {
+  global: (props: StyleFunctionProps) => ({
+    body: {
+      // sets a custom bg color for dark mode only
+      bg: mode(
+        // light mode value retrieved from theme
+        '#d0d0d0',
+        // your custom value for dark mode
+        '#0b0b0b',
+      )(props),
+    },
+  }),
+};
+
+
+const theme = extendTheme({
+  config,
+  colors,
+  styles,
+});
 
 export default theme;
+
